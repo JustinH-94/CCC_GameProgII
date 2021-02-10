2b083b7c9cef7cbdf4e640ab4fbc5af0dde13aab
JustinH-94 <61295115+JustinH-94@users.noreply.github.com>
2021-02-09 20:56:23 -0600

    Singleton Demo in Unity

---
+++ Assets/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class Singleton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static Singleton instance;
+
+    protected Singleton() { }
+    public string value;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<Singleton>();
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
+
+public class TextRetrieve
+{
+    TextAsset textAsset;
+
+    public TextRetrieve()
+    {
+    }
+
+    public void DataRetrieval()
+    {
+        Singleton.Instance.value = Application.persistentDataPath;
+        textAsset = Resources.Load(Singleton.Instance.value) as TextAsset;
+    }
+}
